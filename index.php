ac121ab18dc1ce0974aa255d33ae54ba849cf1ea
sarzafi <nabilakhairunisa0001@gmail.com>
2017-06-14 15:16:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,80 +1,46 @@
-<?php
-echo "<h1> Contoh Beberapa Tabel PHP </h1>";
-echo "<hr>";
-echo "Tabel satu kolom";
-echo "<table width = 500 border =1>
-<tr>
-<td> Tabel satu kolom </td>
-</tr>
-</table>";
-?>
-<?php
-echo "<br>";
-echo "Tabel dua kolom";
-echo "<table width = 500 border =1>
-<tr>
-<td> kolom pertama </td>
-<td> kolom ke dua </td>
-</tr>
-</table>";
-
-?>
-<?php
-echo "<br>";
-echo "Tabel dua baris";
-echo "<br>";
-echo "<table width = 500 border =1>
-<tr>
-<td>baris pertama</td>
-</tr>
-<tr>
-<td>baris kedua</td>
-</tr>
-
-</table>";
-?>
-<?php
-echo "<br>";
-echo "Tabel dua baris dua kolom";
-echo "<br>";
-echo "<table width = 500 border =1>
-<tr>
-<td>baris pertama</td>
-<td>baris ketiga</td>
-</tr>
-<tr>
-<td>baris kedua</td>
-<td>baris ke empat</td>
-</tr>
-</table>";
-?>
-<?php
-echo "<br>";
-echo "Semua TAbel Bewarna";
-echo "<br>";
-echo "<table width = 500 border =1 bgcolor= 00fffff>
-<tr>
-<td>baris pertama</td>
-<td>baris ketiga</td>
-</tr>
-<tr>
-<td>baris kedua</td>
-<td>baris ke empat</td>
-</tr>
-</table>";
-?>
-<?php
-echo "<br>";
-echo "Hanya kolom yang bewarna";
-echo "<br>";
-echo "<table width = 500 border =1>
-<tr bgcolor=00fffff>
-<td>baris pertama</td>
-<td>baris ketiga</td>
-</tr>
-<tr>
-<td>baris kedua</td>
-<td>baris ke empat</td>
-</tr>
-</table>";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat design table bergaya elegan dengan CSS | MalasNgoding.com</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<center><h1>Membuat design table bergaya elegan dengan CSS | MalasNgoding.com</h1></center>
+	<table cellspacing='0'>
+		<thead>
+			<tr>
+				<th>Nama Anggota</th>
+				<th>Alamat</th>
+				<th>Pekerjaan</th>
+				<th>Usia</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Andi</td>
+				<td>Jakarta Selatan</td>
+				<td>Web Designer</td>
+				<td>21</td>
+			</tr>
+			<tr>
+				<td>Budi</td>
+				<td>Bandung</td>
+				<td>Web Developer</td>
+				<td>26</td>
+			</tr>
+			<tr>
+				<td>Cahyo</td>
+				<td>Bekasi</td>
+				<td>Web Programmer</td>
+				<td>29</td>
+			</tr>
+			<tr>
+				<td>Darma</td>
+				<td>Bali</td>
+				<td>Teknisi</td>
+				<td>35</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
