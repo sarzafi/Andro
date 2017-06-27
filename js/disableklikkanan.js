27faef5463a14d2c525d86d347960ae690ed5302
sarzafi <nabilakhairunisa0001@gmail.com>
2017-06-27 21:38:52 +0700

    Update and rename sp_tilang.js to disableklikkanan.js

---
+++ js/disableklikkanan.js
@@ -0,0 +1,24 @@
+<script type="text/javascript">
+  var message="MASUKKAN PESAN KAMU DISINI!";
+  function clickIE4(){
+    if(event.button==2){
+      alert(message);
+      return false;
+    }
+  }
+  function clickNS4(e){
+    if(document.layers||document.getElementById&&!document.all){
+      if(e.which==2||e.which==3){
+        alert(message);
+        return false;
+      }
+    }
+  }
+  if(document.layers){
+    document.captureEvents(Event.MOUSEDOWN);
+    document.onmousedown=clickNS4;
+  }else if(document.all&&!document.getElementById){
+    document.onmousedown=clickIE4;
+  }
+  document.oncontextmenu=new Function("alert(message);return false");
+</script><!--IE=internet explorer 4+ dan NS=netscape 4+0-->
